d87bfcdc3566deec8947b6d6262dd27f71901632
cindyk0705 <cindyk0705@gmail.com>
2025-09-12 15:42:27 -0400

    added in bsky template

---
+++ Communication Subproblem/app_template.py
@@ -0,0 +1,27 @@
+import gspread
+import atproto
+from oauth2client.service_account import ServiceAccountCredentials
+from atproto import Client
+
+# Define the scope
+scope = [
+    'https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/drive'
+]
+
+# Authenticate with credentials
+credentials = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+client = gspread.authorize(credentials)
+
+# Open the Google Sheet
+sheet = client.open('Your Google Sheet Name').sheet1
+
+# Fetch the first row of data
+first_row = sheet.row_values(1)
+print(f"First row of data: {first_row}")
+
+# bluesky connection
+client = Client()
+client.login('marbledmonsoon.bsky.social','aF9E5BQgx0rRfS')
+
+post = client.send_post('Hello world! I posted this via the Python SDK.')
